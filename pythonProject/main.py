efb9c97d8d6270d616d3f30f484b807a9c8759fb
Dawid Kaczmarczyk <125995212+Xayren272@users.noreply.github.com>
2024-10-02 10:29:12 +0200

    Program overhaul

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,6 +1,11 @@
+from sys import prefix
+
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver import Keys
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.actions.action_builder import ActionBuilder
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.relative_locator import locate_with
 
 driver = webdriver.Chrome()
 driver.get("https://www.typescriptlang.org/")
@@ -16,10 +21,34 @@ print(driver.title)
 print(h2.get_attribute("innerHTML"))
 assert "What is TypeScript?" in h2.get_attribute("innerHTML")
 
-elem = driver.find_element(By.ID, "search-box-top")
-elem.click()
-elem.send_keys("interface")
-elem.send_keys(Keys.ENTER)
+assert "https://www.typescriptlang.org/" in driver.current_url
 
-assert "TypeScript: Documentation - DOM Manipulation" in driver.title
+link_finder = locate_with(By.TAG_NAME, "a").near({By.CLASS_NAME: "call-to-action"})
+button1 = driver.find_element(link_finder)
+button1.click()
+
+button2 = driver.find_element(link_finder)
+ActionChains(driver)\
+    .click(button2)\
+    .pause(4)\
+    .perform()
+
+ActionBuilder(driver).clear_actions()
+
+assert "https://www.typescriptlang.org/play" in driver.current_url
+
+input_field = driver.find_element(By.CLASS_NAME, "inputarea")
+ActionChains(driver)\
+    .click(input_field)\
+    .key_down(Keys.CONTROL)\
+    .send_keys("a")\
+    .pause(1)\
+    .key_up(Keys.CONTROL)\
+    .send_keys("const test = true;")\
+    .pause(2)\
+    .perform()
+
+ActionBuilder(driver).clear_actions()
+
+
 driver.close()
